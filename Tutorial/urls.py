c14ad8ac47cb7c66e46cfba5ff2b5601c3057d83
mkbguillermo-dev <mkbguillermo@gmail.com>
2024-09-18 16:01:50 +0800

    modified templates to display posts from database.

--- Tutorial/urls.py
+++ Tutorial/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
+    # path('', views.home, name='index'),
+    path('', views.post_list, name='post_list'),
 ]
